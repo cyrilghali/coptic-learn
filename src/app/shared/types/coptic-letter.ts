44cef10df1f8c0a8f4863b8c58b5d486df89738c
Cyril Ghali <contact@cyrilghali.fr>
2024-07-11 11:43:23 +0200

    move constants and types

---
+++ src/app/shared/types/coptic-letter.ts
@@ -0,0 +1,18 @@
+export interface copticLetter {
+    uppercaseImage: string;
+    lowercaseImage: string;
+    uppercaseUnicode: string;
+    lowercaseUnicode: string;
+    numericValue: number | null;
+    letterName: string;
+    greekEquiv: string;
+    transliteration: string;
+    sahidicPron: string;
+    bohairicPron: string;
+    lateCopticPron: string;
+    grecoBohairicPron: string;
+    displayFontLetter: string;
+    sound: {
+        pronunciation: string;
+    };
+}
